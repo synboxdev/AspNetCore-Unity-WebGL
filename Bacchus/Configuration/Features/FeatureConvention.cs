c48274d25368c02c02cce03d303b7ae8ebf6da2d
Synbox <60240550+synboxdev@users.noreply.github.com>
2024-12-06 23:18:46 +0200

    no message

---
+++ Bacchus/Configuration/Features/FeatureConvention.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System.Reflection;
+
+namespace Bacchus.Configuration.Features;
+
+public class FeatureConvention : IControllerModelConvention
+{
+    public void Apply(ControllerModel controller)
+    {
+        string? featureName = GetFeatureName(controller.ControllerType);
+        controller.Properties.Add("feature", featureName);
+    }
+
+    private static string? GetFeatureName(TypeInfo controllerType)
+    {
+        string[]? tokens = !string.IsNullOrEmpty(controllerType.FullName) ? controllerType.FullName.Split('.') : null;
+
+        if (tokens is not null && !tokens.Any(t => t == "Features"))
+            return string.Empty;
+
+        string featureName = tokens
+            .SkipWhile(t => !t.Equals("features", StringComparison.CurrentCultureIgnoreCase))
+            .Skip(1)
+            .Take(1)
+            .FirstOrDefault();
+
+        return featureName;
+    }
+}
---
+++ Bacchus/Configuration/Razor/StaticFileExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.StaticFiles;
+
+namespace Bacchus.Configuration.Razor;
+
+public static class StaticFileExtensions
+{
+    public static FileExtensionContentTypeProvider GetFileExtensionContentTypeProvider()
+    {
+        var provider = new FileExtensionContentTypeProvider();
+
+        provider.Mappings.Add(".unityweb", "application/octet-stream");
+        provider.Mappings.Add(".data", "application/octet-stream");
+        provider.Mappings.Add(".br", "application/brotli");
+        provider.Mappings.Add(".data.br", "application/brotli");
+        provider.Mappings.Add(".wasm.br", "application/wasm");
+        provider.Mappings.Add(".js.br", "application/javascript");
+        provider.Mappings.Add(".symbols.js.br", "application/octet-stream");
+        provider.Mappings.Add(".bundle", "application/octet-stream");
+
+        return provider;
+    }
+}
